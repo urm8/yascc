90c43f2a6ade819606d9cf9053368227c0fa78a4
urm8 <arctgisinil@gmail.com>
2021-07-06 23:12:48 +0300

    Squashed commit of the following:
    commit 6c944175c11517244475ab2add4cbbb4ca23bf06
    Author: urm8 <arctgisinil@gmail.com>
    Date:   Tue Jul 6 23:12:41 2021 +0300
    
        bump ver
    
    commit 82b030afaac5e36f9fbf38c1f63963ae1ec0acaa
    Merge: 1403f30 a9a7ab2
    Author: urm8 <arctgisinil@gmail.com>
    Date:   Tue Jul 6 23:09:18 2021 +0300
    
        Merge branch 'feature/support-nested-dict-list' into develop
    
    commit a9a7ab28b28700e02eb1b43b36ca080186f73a06
    Author: urm8 <arctgisinil@gmail.com>
    Date:   Tue Jul 6 23:09:02 2021 +0300
    
        upd structure add tests
    
    commit e9c80cd862087c0d3d95e08dabfcd1210d542c78
    Author: urm8 <arctgisinil@gmail.com>
    Date:   Tue Jul 6 22:31:02 2021 +0300
    
        nested stuff

---
+++ tests/test_camelcase.py
@@ -0,0 +1,8 @@
+import pytest
+
+from yascc.camelcase import decamelize
+
+
+def test_decamelize(camelized_dict: dict, decamelized_dict: dict) -> None:
+    got = decamelize(camelized_dict)
+    assert got == decamelized_dict
