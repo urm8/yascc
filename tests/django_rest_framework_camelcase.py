08bc443e54652ee3e950236a8a75b7cc933c1cb5
urm8 <arctgisnil@gmail.com>
2022-03-04 23:36:52 +0300

    reimpl

---
+++ tests/django_rest_framework_camelcase.py
@@ -0,0 +1,74 @@
+import re
+from collections import OrderedDict
+
+camelize_re = re.compile(r"[a-z0-9]?_[a-z0-9]")
+
+
+def underscore_to_camel(match):
+    group = match.group()
+    if len(group) == 3:
+        return group[0] + group[2].upper()
+    else:
+        return group[1].upper()
+
+
+def camelize(data, **options):
+    # Handle lazy translated strings.
+    ignore_fields = options.get("ignore_fields") or ()
+    if isinstance(data, dict):
+        new_dict = OrderedDict()
+        for key, value in data.items():
+            if isinstance(key, str) and "_" in key:
+                new_key = re.sub(camelize_re, underscore_to_camel, key)
+            else:
+                new_key = key
+            if key not in ignore_fields and new_key not in ignore_fields:
+                new_dict[new_key] = camelize(value, **options)
+            else:
+                new_dict[new_key] = value
+        return new_dict
+    if is_iterable(data) and not isinstance(data, str):
+        return [camelize(item, **options) for item in data]
+    return data
+
+
+def get_underscoreize_re(options):
+    if options.get("no_underscore_before_number"):
+        pattern = r"([a-z0-9]|[A-Z]?(?=[A-Z](?=[a-z])))([A-Z])"
+    else:
+        pattern = r"([a-z0-9]|[A-Z]?(?=[A-Z0-9](?=[a-z0-9]|$)))([A-Z]|(?<=[a-z])[0-9](?=[0-9A-Z]|$)|(?<=[A-Z])[0-9](?=[0-9]|$))"
+    return re.compile(pattern)
+
+
+def camel_to_underscore(name, **options):
+    underscoreize_re = get_underscoreize_re(options)
+    return underscoreize_re.sub(r"\1_\2", name).lower()
+
+
+def _get_iterable(data):
+    return data.items()
+
+
+def underscoreize(data, **options):
+    if isinstance(data, dict):
+        new_dict = {}
+        for key, value in _get_iterable(data):
+            if isinstance(key, str):
+                new_key = camel_to_underscore(key, **options)
+            else:
+                new_key = key
+            new_dict[new_key] = value
+        return new_dict
+    if is_iterable(data):
+        return [underscoreize(item, **options) for item in data]
+
+    return data
+
+
+def is_iterable(obj):
+    try:
+        iter(obj)
+    except TypeError:
+        return False
+    else:
+        return True
--- tests/test_camelcase.py
+++ tests/test_camelcase.py
@@ -1,6 +1,15 @@
+import pytest
+
 from yascc.camelcase import decamelize
+
+from .django_rest_framework_camelcase import underscoreize
 
 
 def test_decamelize(camelized_dict: dict, decamelized_dict: dict) -> None:
     got = decamelize(camelized_dict)
     assert got == decamelized_dict
+
+
+@pytest.mark.parametrize("impl", [decamelize, underscoreize], ids=["c", "drf"])
+def test_decamelize_benchmark(benchmark, camelized_dict, impl):
+    benchmark(impl, camelized_dict)
