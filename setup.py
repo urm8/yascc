bdcf79ca6ee32f02708f117984b0251daf4e7dec
urm8 <arctgisnil@gmail.com>
2022-04-23 23:14:49 +0400

    add builder

--- setup.py
+++ setup.py
@@ -1,7 +1,15 @@
+import logging
 import sys
+from distutils.command.build_ext import build_ext
+from distutils.core import Extension
+from distutils.errors import CCompilerError
+from distutils.errors import DistutilsExecError
+from distutils.errors import DistutilsPlatformError
 
 from setuptools import Extension
 from setuptools import setup
+
+log = logging.getLogger(__name__)
 
 args = []
 if sys.platform == "win32":
@@ -12,4 +20,25 @@ else:
 ext_modules = [Extension("_case", sources=["src/yascc/case.c"], extra_compile_args=args)]
 
 
-setup(ext_modules=ext_modules)
+class ExtBuilder(build_ext):
+    def run(self):
+        try:
+            build_ext.run(self)
+        except (DistutilsPlatformError, FileNotFoundError) as e:
+            log.error("Could not compile C extension.")
+            raise e from None
+
+    def build_extension(self, ext):
+        try:
+            build_ext.build_extension(self, ext)
+        except (
+            CCompilerError,
+            DistutilsExecError,
+            DistutilsPlatformError,
+            ValueError,
+        ) as e:
+            log.error("Could not compile C extension.")
+            raise e from None
+
+
+setup(ext_modules=ext_modules, cmdclass={"build_ext": ExtBuilder})
